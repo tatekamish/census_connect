1e7ee8c4148a12bb3bf8b0b588831c308cafe256
stfran22 <81263460+stfran22@users.noreply.github.com>
2022-04-18 12:11:03 -0400

    Add files via upload
    this is the second one

--- function_edits.py
+++ function_edits.py
@@ -27,10 +27,9 @@ def get_table(get_vars, pred_for, pred_in, year):
     df.to_csv("table.csv", quoting = QUOTE_NONNUMERIC, index=False)
     out_file = open("table.csvt", "w")
     csvt_text = '"Integer",' * len(get_vars) + '"String",' * (len(geocols)) + '"String"'
-    #csvt_text = '''"Integer","String","String","String"'''
     out_file.write(csvt_text)
     out_file.close()
-    return 
+    return df
 
 df = get_table(["P001001"], "county", "42", "2010")
 
@@ -44,19 +43,6 @@ df = get_table(["P001001"], "county", "42", "2010")
 
 # add csv to QGIS project
 
-count = 0
-def code_to_run():
-  print("Running code")
-  global count
-  count +=1
-
-while True:
-  print("You have run the code {} times".format(count))
-  run = str(input("Shall I run the code? (y/n) "))
-  if run == "y":
-    code_to_run()
-  else:
-    break
 
 
 #pop_table = r'file:///C:/Users/tuo25083/Documents/app_dev/census_connect/pa_county_pop.csv?delimiter=,'
@@ -77,34 +63,29 @@ def fetchZip(url, project_home):
     with open(outFileName, 'wb') as outf:
         outf.write(binContents)
     
-
+# remove path from get_geo, see if file gets saved to folder
 #Get Geometry  (Function not completely finished--mostly unsure of local/global variables and what all needs to be in the function since theres a get zip and build url function)
 def get_geo(geo_level, year, state):
 
     url_prefix="https://www2.census.gov/geo/tiger/" #Not User Input
     url_tiger= "TIGER{}".format(year)           # "TIGER####"
-    if geo_level == "*":
-        geo_level = "COUNTY"
-    else:
-        pass
-    url_geo_level = geo_level    # "COUNTY", "STATE", etc. 
+    url_geo_level = geo_level.upper()    # "COUNTY", "STATE", etc. 
     url_year = year            # "####"
     url_state = state          #2-dig state code
-    url_geo_lower = url_geo_level.lower() + f"{year[2:]}" #used in url_suffix (file name)
+    url_geo_lower = geo_level + f"{year[2:]}" #used in url_suffix (file name)
     url_suffix="tl_{0}_{1}_{2}.zip".format(url_year,url_state,url_geo_lower)
     shp_join_field='GEOID{}'.format(year[2:])
     #not sure if this needs to be global for future referance
     geo_url = "{0}{1}/{2}/{3}/{4}".format(url_prefix, url_tiger, url_geo_level, url_year, url_suffix)
     #STATE ex: https://www2.census.gov/geo/tiger/TIGER2010/STATE/2010/tl_2010_01_state10.zip
-    #theURL = 'https://www2.census.gov/geo/tiger/TIGER2010/COUNTY/2010/tl_2010_42_county10.zip' #original grab
-    
+    #theURL = 'https://www2.census.gov/geo/tiger/TIGER2010/COUNTY/2010/tl_2010_42_county10.zip' #original grab 
     fetchZip(geo_url, project_home) #change to geo_url once user input final
     print('{0}{1} created.'.format(project_home, os.path.basename(geo_url)))
     return geo_url, url_suffix, shp_join_field
     print("FOR GIT HUB TO RECOGNIZE AS UPDATED TEXT DELTE LATER")
 
 # TEST:
-geo_url, url_suffix, shp_join_field  = get_geo('*', '2010', '42')
+geo_url, url_suffix, shp_join_field  = get_geo('county', '2010', '42')
 print(shp_join_field)
 
 #add shapefile to QGIS project
@@ -113,7 +94,7 @@ zip_path = '/vsizip/' + project_home + os.path.basename(geo_url)
 shp = QgsVectorLayer(zip_path, url_suffix[:-4], 'ogr')
 QgsProject.instance().addMapLayer(shp)
 
-shp_join_field='GEOID10'
+
 
 #make a function to join csv to shapefile
 def join_to_geog (shp, csv, shp_join_field):   
